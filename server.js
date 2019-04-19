35e5c25ae12d448ade51c1634d9735017bd92716
ulyashev <alexeyulyashev@gmail.com>
2019-04-19 15:31:47 +0300

    add method "get list of products"

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
-// server.js
-const express        = require('express');
-const MongoClient    = require('mongodb').MongoClient;
-const bodyParser     = require('body-parser');
-const app            = express();
+// const MongoClient    = require('mongodb').MongoClient;
+// const bodyParser     = require('body-parser');
 
-//var http  =require('http');
-//var server = http.createServer(function (req, res) {
-//
-//});
+var express = require("express");
+var bodyParser = require("body-parser");
+var fs = require("fs");
+ 
+var app = express();
+var jsonParser = bodyParser.json();
+ 
+app.use(express.static(__dirname + "/public"));
+// get list of products
+app.get("/api/products", function(req, res){
+      
+    var content = fs.readFileSync("products.json"); //, "utf8");
+    var products = JSON.parse(content);
+    res.send(products);
+});
 
-const port = 8000;
-app.listen(port, () => {
-  console.log('We are live on ' + port);
+
+  
+app.listen(3000, function(){
+    console.log("Server run on 3000");
 });
