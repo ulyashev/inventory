8594ee2cd6e33fde01c635022a919d6785be5561
ulyashev <alexeyulyashev@gmail.com>
2019-04-19 15:33:07 +0300

    add method "get product by id"

--- server.js
+++ server.js
@@ -17,6 +17,30 @@ app.get("/api/products", function(req, res){
     res.send(products);
 });
 
+// get one product by id
+app.get("/api/product/:id", function(req, res){
+      
+  var id = req.params.id; // get id
+  var content = fs.readFileSync("products.json"); //, "utf8");
+  var products = JSON.parse(content);
+  var product = null;
+  // get user by id
+  for(var i=0; i<products.length; i++){
+      if(products[i].id==id){
+          product = products[i];
+          break;
+      }
+  }
+  // send product
+  if(product){
+      res.send(product);
+      console.log(product);
+  }
+  else{
+    console.log('404');
+    res.status(404).send();
+  }
+});
 
   
 app.listen(3000, function(){
