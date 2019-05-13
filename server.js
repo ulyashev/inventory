4044f6baca7c96929880e5bcabe07d670bdc86ce
ulyashev <alexeyulyashev@gmail.com>
2019-05-13 12:15:00 +0300

    refactoring GET by ID

--- server.js
+++ server.js
@@ -65,28 +65,13 @@ app.get("/api/products", function(req, res){
 });
 
 // get one product by id
-app.get("/api/product/:id", function(req, res){
-      
+app.get("/api/products/:id", function(req, res){
   var id = req.params.id; // get id
-  var content = fs.readFileSync("products.json"); //, "utf8");
-  var products = JSON.parse(content);
-  var product = null;
-  // get user by id
-  for(var i=0; i<products.length; i++){
-      if(products[i].id==id){
-          product = products[i];
-          break;
-      }
-  }
-  // send product
-  if(product){
-      res.send(product);
-      console.log(product);
-  }
-  else{
-    console.log('404');
-    res.status(404).send();
-  }
+  console.log(id);
+  Product.findOne({_id:id}, function(err, product){
+    if (err) return console.log(err);
+    res.send(product);
+  });
 });
 
 
